355bcde742063ebfb6cf155f8b8042e948035911
Evgeny Simonov <jdsimonov2@gmail.com>
2024-12-14 19:06:24 +0300

    modified:   scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,27 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-function createCard(card){
+function setCard(card){
     const cardTemplate = document.querySelector("#card-template").content;
     const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+    let deleteButton = cardTemplate.querySelector(".card__delete-button");
+    deleteButton.addEventListener('click',deleteCard);
     cardItem.querySelector(".card__image").src = card.link;
     cardItem.querySelector(".card__image").alt = card.name;
     cardItem.querySelector(".card__title").textContent = card.name;
     return cardItem;
 }
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+function createCard(card){
+    let placesList = document.querySelector(".places__list");
+    card = setCard(card);
+    placesList.appendChild(card);
+}
 // @todo: Функция удаления карточки
 function deleteCard(){
-
+    console.log('card deleted');
 }
 // @todo: Вывести карточки на страницу
-console.log(createCard(
-    {
-        name:"Архыз",
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
-    }
-));
+initialCards.forEach(card => {
+    createCard(card);
+});
