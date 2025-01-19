654e29928b8aa6fb98b28a0b17a72cab32a7673e
Evgeny Simonov <jdsimonov2@gmail.com>
2025-01-19 16:48:00 +0300

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 module.exports = {
-    entry: {main:'./scripts/index.js'},
+    entry: {main:'./src/scripts/index.js'},
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
@@ -38,7 +38,7 @@ module.exports = {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html'
+        template: './src/index.html'
       }),
           new CleanWebpackPlugin(),
           new MiniCssExtractPlugin()
